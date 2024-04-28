da11f4ae809cd44ee134af0e3c43b9201c2890d0
JoseMuñoz <oemy19@gmail.com>
2024-04-28 18:18:05 +0200

    time&date

--- cypress/e2e/time_date.cy.js
+++ cypress/e2e/time_date.cy.js
@@ -1,10 +1,29 @@
-describe('Realizar una accion', () => {
-    it('test de login valido', () => {
-        cy.visit('https://www.timeanddate.com/')
-        cy.get(':nth-child(1) > .tad-box__content > .tad-clip-blocktad-clip-block--picker > .bn-header__searchbox > .picker-city__input').type('Barcelona(enter)')
-        cy.get(':nth-child(1) > .tad-box__content > .tad-clip-blocktad-clip-block--picker > .bn-header__searchbox > .picker-city__button > .i-font').click()
-
-
-
+describe('Obtener hora de una ciudad', () => {
+    it('Test de obtener hora de una ciudad', () => {
+        cy.visit('https://automationexercise.com/')
+        cy.get(':nth-child(2) > .panel-heading > .panel-title > a > .badge > .fa').click()
+        cy.get('#Men > .panel-body > ul > :nth-child(2) > a').click()
+        cy.get(':nth-child(3) > .product-image-wrapper > .single-products > .productinfo > .btn').click()
+        cy.get('.modal-footer > .btn').click()
  })
+
+ it('Test de login', () => {
+    cy.visit('https://automationexercise.com/')
+    cy.get('.shop-menu > .nav > :nth-child(4) > a').click()
+    cy.get('[data-qa="signup-name"]').type('Oso')
+    cy.get('[data-qa="signup-email"]').type('oemy19@hotmail.com')
+    cy.get('[data-qa="signup-button"]').click()
+
+}) 
+
+it('Datos login', () => {
+    cy.visit('https://automationexercise.com/')
+    cy.get('#id_gender1').click()
+
+
+
+
+
+})
+
 })
